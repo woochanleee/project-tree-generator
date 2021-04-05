126254d91484d3a7d5a7e6af09479eacdf35fd8d
_uchanlee <uchanlee.dev@gmail.com>
2021-04-06 08:28:43 +0900

    사이트 hits 체커 추가

--- src/Header.tsx
+++ src/Header.tsx
@@ -9,6 +9,9 @@ export function Header() {
             <h1>Projcet Tree Generator</h1>
           </div>
         </a>
+        <a href='https://hits.seeyoufarm.com'>
+          <img src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fwoochanleee.github.io%2Fproject-tree-generator&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false' />
+        </a>
       </div>
     </header>
   );
